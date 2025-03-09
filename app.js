d7806396a882eaef9252031e617859f73072f010
josegomez221 <50353593+josegomez221@users.noreply.github.com>
2025-03-09 06:47:10 -0500

    se crean comentarios

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-let amigos = [];
+let listaAmigos = [];
+
 function agregarAmigo() {
+    //recibe valor de unput con id 'amigo'
     let amigo = document.getElementById('amigo').value;
+    //valida si el input tiene información
     if (amigo === "") {
         alert("Debes ingresar el nombre del amigo");
     }else{
-        amigos.push(amigo);
-        document.querySelector('#amigo').value = '';
+        //ingresa input a lista 'listaAmigos'
+        listaAmigos.push(amigo);
+
+        //limpia input
+        document.getElementById('amigo').value = '';
     }
     
 }
